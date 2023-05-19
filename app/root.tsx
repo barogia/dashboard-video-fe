f4a4e8ab41db5f624c99b81887d1d5f9b65389e0
barokool <knabao7a7@gmail.com>
2023-05-19 13:34:05 +0700

    feat : title of root app

--- app/root.tsx
+++ app/root.tsx
@@ -118,7 +118,7 @@ const Document = withEmotionCache(
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "New Remix App",
+  title: "Dashboard video",
   viewport: "width=device-width,initial-scale=1",
 });
 
